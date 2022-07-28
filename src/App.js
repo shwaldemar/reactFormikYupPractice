d025965757dc2fd8ee3c96d763561c017c39c2aa
Simon Hall <shwaldemar@gmail.com>
2022-07-28 12:35:29 +0100

    added formik and yup depemdencies to manage form input state and validation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { useState } from 'react';
-import BasicForm from './components/BasicForm.js'
-import AdvancedForm from './components/AdvancedForm.js'
-import './App.css';
+import { useState } from "react";
+import BasicForm from "./components/BasicForm";
+import AdvancedForm from "./components/AdvancedForm";
+import "./App.css";
 
 function App() {
-  const [ view, setView ] = useState('basic');
-
+  const [view, setView] = useState("basic");
   return (
     <div className="App">
       <nav>
-        <h3 
-        onClick={setView('basic')}
-        style={{color: view === 'basic' ? "#fff" : ""}}>
-        Basic
+        <h3
+          onClick={() => setView("basic")}
+          style={{ color: view === "basic" ? "#fff" : "" }}
+        >
+          Basic
         </h3>
-        <h3 
-        onClick={setView('advanced')}
-        style={{color: view === 'advanced' ? "#fff" : ""}}>
-        Advanced
+        <h3
+          onClick={() => setView("advanced")}
+          style={{ color: view === "advanced" ? "#fff" : "" }}
+        >
+          Advanced
         </h3>
       </nav>
-      {view === 'basic' ?<BasicForm /> : <AdvancedForm />}
-
+      {view === "basic" ? <BasicForm /> : <AdvancedForm />}
     </div>
   );
 }
--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BasicForm = () => {
+  
   return (
     <div>
     <h1>Basic Form</h1>
       <form autoComplete='off'>
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" placeholder="Enter your email"/>
+        <input  id="email" type="email" placeholder="Enter your email"/>
       </div>
       </form>
     </div>

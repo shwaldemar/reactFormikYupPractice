9825046686fc581fd52d8cc1d711e151551f9843
Simon Hall <shwaldemar@gmail.com>
2022-07-28 12:59:56 +0100

    basic form uses formik to accept and alert json of user input

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useFormik } from 'formik';
 
 const BasicForm = () => {
-  
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: values => alert(JSON.stringify(values, null, 2)),
+  })
+  console.log(formik);
   return (
     <div>
     <h1>Basic Form</h1>
-      <form autoComplete='off'>
+      <form autoComplete='off' onSubmit={formik.handleSubmit}>
       <div>
         <label htmlFor="email">Email</label>
-        <input  id="email" type="email" placeholder="Enter your email"/>
+        <input 
+        value={formik.values.email} 
+        onChange={formik.handleChange}
+        id="email" 
+        type="email" 
+        placeholder="Enter your email"/>
       </div>
+      <button type='submit'>Submit</button>
       </form>
     </div>
   )
